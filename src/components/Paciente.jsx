f748d4e9093b6173e9a23adbea43137e15891ef4
David <pablodavidhuamani@gmail.com>
2022-07-15 21:56:18 -0500

    Agregando Botones editar y eliminar

--- src/components/Paciente.jsx
+++ src/components/Paciente.jsx
@@ -3,7 +3,7 @@ const Paciente = ({paciente}) => {
   return (
     <div className="mx-5 my-10 bg-white shadow-md px-5 py-10 rounded-xl">
             <p className='font-bold mb-3 text-gray-700 uppercase
-            '>Nombre:{""}
+            '>Nombre Mascota:{""}
                <span className='font-normal normal-case'>{nombre}</span>
             </p>
 
@@ -23,6 +23,18 @@ const Paciente = ({paciente}) => {
             '>Sintomas:{""}
                <span className='font-normal normal-case'>{sintomas}</span>
             </p>
+            <div className="flex justify-between mt-10">
+               <button type="button"
+               className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold rounded-full">
+                  Editar
+
+               </button>
+               <button type="button"
+               className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold rounded-full">
+                  Eliminar
+
+               </button>
+            </div>
 
         </div>
   )
